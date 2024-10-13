799c83c1d14d60ed9cbc12473b3f40cdc3e9a154
Dechasaejara <dechasaejara@gmail.com>
2024-10-13 18:05:53 +0300

    fix bug

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -3,6 +3,7 @@ export const fetchCache = 'force-no-store'
 import { fetchTelegramUpdates } from '@/app/actions/getupdate'
 import { TelegramGetUpdate } from '@/lib/db/type'
 import { Bot, Keyboard, webhookCallback } from 'grammy'
+import { useRouter } from 'next/navigation';
 
 const token = process.env.TELEGRAM_BOT_TOKEN
 
@@ -10,6 +11,7 @@ if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.'
 const channelId = 1670159978; // Replace with your channel ID
 const keyboard = new Keyboard().text('Show Status').text('Post').row().text('Join our community')
 const bot = new Bot(token)
+
 bot.command("start", async (ctx) => {
   const user = ctx.message?.from.username; // the message object
   await ctx.reply(`<b>Hello <i>${user}</i> , welcome to our bot.</b>`, { reply_markup: keyboard, parse_mode: "HTML" });
@@ -23,11 +25,12 @@ const handleMessageText = async (ctx: any) => {
     return await ctx.reply("Click here to join our main channel:    https://t.me/+5IyRGnhrSvcxNmU0", { reply_markup: keyboard });
   }
   if (message.text === "Post") {
-    const data = await fetchTelegramUpdates(token)
-    if (data.ok) {
-      // await sendMessageToChannel(data.result[0].channel_post.chat.id, data.result[0].channel_post.text)
-      return await ctx.reply(`New message posted ${JSON.stringify(data)}`, { reply_markup: keyboard });
-    }
+    // const data = await fetch(`https://api.telegram.org/bot${token}/getUpdates`);
+    // if (data.ok) {
+    //   // await sendMessageToChannel(data.result[0].channel_post.chat.id, data.result[0].channel_post.text)
+    //   return await ctx.reply(`New message posted ${JSON.stringify(data)}`, { reply_markup: keyboard });
+    // }
+    await sendMessageToChannel(-1001670159978, "First post from bot");
   }
   return await ctx.reply(message);
 };
