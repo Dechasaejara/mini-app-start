ba2b76ca7348d6aed66019ceb774927544a5f79f
Dechasaejara <dechasaejara@gmail.com>
2024-10-13 16:00:48 +0300

    test channel post

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -5,8 +5,8 @@ import { Bot, Keyboard, webhookCallback } from 'grammy'
 const token = process.env.TELEGRAM_BOT_TOKEN
 
 if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
-
-const keyboard = new Keyboard().text('Show Status').text('Add').row().text('Join our community')
+const channelId = 1670159978; // Replace with your channel ID
+const keyboard = new Keyboard().text('Show Status').text('Post').row().text('Join our community')
 const bot = new Bot(token)
 bot.command("start", async (ctx) => {
   const user = ctx.message?.from.username; // the message object
@@ -20,10 +20,21 @@ const handleMessageText = async (ctx: any) => {
   if (message.text === "Join our community") {
     return await ctx.reply("Click here to join our main channel:    https://t.me/+5IyRGnhrSvcxNmU0", { reply_markup: keyboard });
   }
-  if (message.text === "Add") {
-    return await ctx.reply(`Please enter amount `, { reply_markup: keyboard });
+  if (message.text === "Post") {
+    await sendMessageToChannel("New message posted ")
+    return await ctx.reply(`New message posted `, { reply_markup: keyboard });
   }
-  return   await ctx.reply(message);
+  return await ctx.reply(message);
+};
+
+// Function to send a message to the channel
+const sendMessageToChannel = async (message: string) => {
+  try {
+    await bot.api.sendMessage(channelId, message);
+    console.log('Message sent to channel:', message);
+  } catch (error) {
+    console.error('Error sending message:', error);
+  }
 };
 
 
